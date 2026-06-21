aa8b741651a34aa716589a9ce6c58b85b47a7410
randomstuffacc1-crypto <randomstuffacc1@gmail.com>
2026-06-21 18:13:33 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'siege-surge-v1';
-const ASSETS = ['./','./index.html','./styles.css','./src/game.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
+const CACHE = 'siege-surge-v2';
+const ASSETS = ['./','./index.html','./styles.css','./game.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
